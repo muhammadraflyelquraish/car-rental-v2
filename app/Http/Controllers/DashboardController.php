c0d7489b6edadc6b30286aec81df1da9f0c7fa2e
Muhammad Rafly El Quraish <m.rafly@kuncicoin.com>
2023-12-25 16:29:53 +0700

    feat: dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Car;
+use App\Models\Customer;
+use App\Models\Driver;
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     function index()
     {
-        return view('admin.dashboard');
+        // Order
+        $totalOrder = Order::count();
+        $activeOrder = Order::where('order_status', 'On Going')->count();
+        $waitingOrder = Order::where('order_status', 'Waiting For Payment')->count();
+        $pickupOrder = Order::where('order_status', 'Waiting For Pickup')->count();
+        $cancelOrder = Order::where('order_status', 'Canceled')->count();
+
+        // Car
+        $totalCar = Car::count();
+        $activeCar = Order::whereIn('order_status', ['On Going', 'Waiting For Payment', 'Waiting For Pickup'])->count();
+        $availableCar = $totalCar - $activeCar;
+
+        // Driver
+        $totalDriver = Driver::count();
+        $activeDriver = Driver::where('status', 'Active')->count();
+        $nonActiveDriver = Driver::where('status', 'Nonactive')->count();
+
+        // Customer
+        $totalCustomer = Customer::count();
+        $approvedCustomer = Customer::where('status_approval', 'Approved')->count();
+        $waitingCustomer = Customer::where('status_approval', 'On Procces')->count();
+
+        // User
+        $totalUser = User::count();
+
+        return view('admin.dashboard', compact(
+            'totalOrder',
+            'activeOrder',
+            'waitingOrder',
+            'pickupOrder',
+            'cancelOrder',
+            'totalCar',
+            'activeCar',
+            'availableCar', 
+            'totalDriver',
+            'activeDriver',
+            'nonActiveDriver',
+            'totalCustomer',
+            'approvedCustomer',
+            'waitingCustomer',
+            'totalUser',
+        ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,172 @@
 @extends('admin.layouts.master')
 
 @section('content')
+<div class="wrapper wrapper-content">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Order</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($totalOrder, 0, ',', ' '); }}</h1>
+                    <small>Total Order</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Mobil</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($totalCar, 0, ',', ' '); }}</h1>
+                    <small>Total Mobil</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Driver</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($totalDriver, 0, ',', ' '); }}</h1>
+                    <small>Total Driver</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-2">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5><small>(Order)&nbsp;Sedang&nbsp;Berjalan</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($activeOrder, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5><small>(Order)&nbsp;Menunggu&nbsp;Pembayaran</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($waitingOrder, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5><small>(Mobil)&nbsp;Disewaan</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($activeCar, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5><small>(Mobil)&nbsp;Tersedia</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($availableCar, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5><small>(Driver)&nbsp;Aktif</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($activeDriver, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5><small>(Driver)&nbsp;Nonactive</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($nonActiveDriver, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-2">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5><small>(Order)&nbsp;Menunggu&nbsp;Pengambilan</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($pickupOrder, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5><small>(Order)&nbsp;Dibatalkan</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($cancelOrder, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>Customer</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($totalCustomer, 0, ',', ' '); }}</h1>
+                    <small>Total Customer</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5>User</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($totalUser, 0, ',', ' '); }}</h1>
+                    <small>Total User</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-2">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h5><small>(Customer)&nbsp;Approved</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($approvedCustomer, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="ibox">
+                <div class="ibox-title">
+                    <h5><small>(Customer)&nbsp;Menunggu&nbsp;Persetujuan</small></h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ number_format($waitingCustomer, 0, ',', ' '); }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
